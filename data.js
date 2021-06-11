359f384404dd4670866978185095dbed3ee90b0a
GIGA360BR <renato.zerati@gmail.com>
2021-06-11 10:11:05 -0300

    primeiro commit

---
+++ data.js
@@ -0,0 +1,665 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---coopec---infantil",
+      "name": "01 - COOPEC - Infantil",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.45460064415914303,
+        "pitch": 0.0718664494735215,
+        "fov": 0.6935710325485662
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5234453006674471,
+          "pitch": 0.09738630969201267,
+          "rotation": 0,
+          "target": "1-02---entrada-infantil"
+        },
+        {
+          "yaw": 2.247216039137334,
+          "pitch": -0.1226631606363835,
+          "rotation": 3.9269908169872414,
+          "target": "1-02---entrada-infantil"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---entrada-infantil",
+      "name": "02 - Entrada Infantil",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.2846982914424441,
+        "pitch": 0.05604735318710752,
+        "fov": 0.8227954580025816
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04912208225436032,
+          "pitch": 0.16194974664390216,
+          "rotation": 0,
+          "target": "2-03---parquinho"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---parquinho",
+      "name": "03 - Parquinho",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.4289205187191385,
+        "pitch": 0.13858486652546986,
+        "fov": 0.8067267112359802
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12923785994916592,
+          "pitch": 0.07007945006068894,
+          "rotation": 1.5707963267948966,
+          "target": "3-04---parquinho"
+        },
+        {
+          "yaw": -2.5572039690768307,
+          "pitch": -0.005858016153055701,
+          "rotation": 5.497787143782138,
+          "target": "3-04---parquinho"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---parquinho",
+      "name": "04 - Parquinho",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -1.8400089623638394,
+        "pitch": 0.055590245954451944,
+        "fov": 0.9951114653170993
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5688851139774762,
+          "pitch": 0.14692535060140344,
+          "rotation": 1.5707963267948966,
+          "target": "4-05---biblioteca"
+        },
+        {
+          "yaw": 1.5814830953805021,
+          "pitch": 0.09672753754181684,
+          "rotation": 0,
+          "target": "4-05---biblioteca"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---biblioteca",
+      "name": "05 - Biblioteca",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -1.4808399918131343,
+        "pitch": 0.047152994255224456,
+        "fov": 0.8369847537565777
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5103286380792156,
+          "pitch": 0.16375962563596325,
+          "rotation": 12.566370614359176,
+          "target": "5-06---biblioteca"
+        },
+        {
+          "yaw": 1.5511994374414346,
+          "pitch": -0.015044679097956148,
+          "rotation": 3.141592653589793,
+          "target": "5-06---biblioteca"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---biblioteca",
+      "name": "06 - Biblioteca",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.6335588762430824,
+        "pitch": 0.02563554381858424,
+        "fov": 0.9139134377360857
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09088018126154651,
+          "pitch": -0.033451159371644223,
+          "rotation": 1.5707963267948966,
+          "target": "6-07---brinquedoteca"
+        },
+        {
+          "yaw": 1.0685685830502756,
+          "pitch": 0.08522342003173478,
+          "rotation": 4.71238898038469,
+          "target": "6-07---brinquedoteca"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07---brinquedoteca",
+      "name": "07 - Brinquedoteca",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": 0.1705586936044554,
+        "pitch": 0.09622184208046392,
+        "fov": 0.8018486840038743
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4807324109429949,
+          "pitch": 0.0878542638456743,
+          "rotation": 1.5707963267948966,
+          "target": "7-08---ptio-infantil"
+        },
+        {
+          "yaw": 2.4373041061305782,
+          "pitch": 0.03827369979970641,
+          "rotation": 4.71238898038469,
+          "target": "7-08---ptio-infantil"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08---ptio-infantil",
+      "name": "08 - Pátio Infantil",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": 0.3705983199168905,
+        "pitch": 0.041587404891025415,
+        "fov": 0.775856623532613
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.849107609111206,
+          "pitch": -0.08022891807937427,
+          "rotation": 7.0685834705770345,
+          "target": "8-09---maternal"
+        },
+        {
+          "yaw": -1.1687512004807523,
+          "pitch": 0.06944906362728176,
+          "rotation": 3.141592653589793,
+          "target": "8-09---maternal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09---maternal",
+      "name": "09 - Maternal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.9818830152034579,
+        "pitch": 0.10701846092471712,
+        "fov": 0.7968899590900975
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.49094321206979785,
+          "pitch": 0.07811306475867852,
+          "rotation": 0.7853981633974483,
+          "target": "9-10---mini-maternal"
+        },
+        {
+          "yaw": 1.9046396548437885,
+          "pitch": 0.055668844518654836,
+          "rotation": 4.71238898038469,
+          "target": "9-10---mini-maternal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10---mini-maternal",
+      "name": "10 - Mini Maternal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.09572623702220007,
+        "pitch": 0.050730797564792596,
+        "fov": 0.9374169115232778
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5492152285318515,
+          "pitch": 0.10536077730901638,
+          "rotation": 0.7853981633974483,
+          "target": "10-11---sala-de-msica"
+        },
+        {
+          "yaw": 2.6198108164572282,
+          "pitch": -0.08697949846322928,
+          "rotation": 3.9269908169872414,
+          "target": "10-11---sala-de-msica"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11---sala-de-msica",
+      "name": "11 - Sala de Música",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -1.2570889591593755,
+        "pitch": 0.7630179038304021,
+        "fov": 1.1103922658198286
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.44080571393567247,
+          "pitch": 0.3192850935969709,
+          "rotation": 0.7853981633974483,
+          "target": "11-12---sala-de-msica"
+        },
+        {
+          "yaw": 2.159894747875266,
+          "pitch": 0.143011733839554,
+          "rotation": 4.71238898038469,
+          "target": "11-12---sala-de-msica"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-12---sala-de-msica",
+      "name": "12 - Sala de Música",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.15611482643524255,
+        "pitch": 0.20000762062136346,
+        "fov": 0.8297966530811779
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14943198842140504,
+          "pitch": -0.08711291321062298,
+          "rotation": 0.7853981633974483,
+          "target": "12-13---pr-i-e-ii"
+        },
+        {
+          "yaw": -1.4130509892755185,
+          "pitch": 0.10019636797624187,
+          "rotation": 0,
+          "target": "12-13---pr-i-e-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-13---pr-i-e-ii",
+      "name": "13 - Pré I e II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.884017532464183,
+        "pitch": 0.12772929454764714,
+        "fov": 0.9521743827224756
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7591579587619979,
+          "pitch": 0.17247558764762871,
+          "rotation": 0,
+          "target": "13-14---pr-i-e-ii"
+        },
+        {
+          "yaw": 0.6280360259357778,
+          "pitch": 0.09809418026790695,
+          "rotation": 5.497787143782138,
+          "target": "13-14---pr-i-e-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-14---pr-i-e-ii",
+      "name": "14 - Pré I e II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.6181956265500528,
+        "pitch": 0.31447533959994267,
+        "fov": 0.816106406573603
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.014687132893648425,
+          "pitch": 0.189756881262646,
+          "rotation": 0.7853981633974483,
+          "target": "14-15---anfiteatro"
+        },
+        {
+          "yaw": 2.3703541160733916,
+          "pitch": 0.04354658365159558,
+          "rotation": 0.7853981633974483,
+          "target": "14-15---anfiteatro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-15---anfiteatro",
+      "name": "15 - Anfiteatro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.4265703218761523,
+        "pitch": 0.02847765365422461,
+        "fov": 1.0091621231126517
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.024142092744478205,
+          "pitch": -0.12058642477031789,
+          "rotation": 1.5707963267948966,
+          "target": "0-01---coopec---infantil"
+        },
+        {
+          "yaw": 2.633458741434799,
+          "pitch": -0.03174085675000882,
+          "rotation": 4.71238898038469,
+          "target": "0-01---coopec---infantil"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
